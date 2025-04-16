f1af2d115e46450f62ac335e7fedd5f801be864f
Ishan Katpally <ishankatpally@cs.ucsb.edu>
2025-04-16 12:57:41 -0700

    implement loss

--- model.py
+++ model.py
@@ -77,11 +77,4 @@ class Yolov1(nn.Module):
             nn.LeakyReLU(0.1),
             nn.Linear(496, S*S*(C+B*5))
         )
-    
-def test(S=7, B=2, C=20):
-    model = Yolov1(split_size=S, num_boxes=B, num_classes=C)
-    x = torch.randn((2, 3, 448, 448))
-    print(model(x).shape)
-
-test()
 
